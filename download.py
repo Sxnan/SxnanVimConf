9765488cbe102f112c822e641a9fa68f539197bf
suxuannan <suxuannan95@gmail.com>
2017-01-07 16:30:48 -0500

    Fix download.py

--- download.py
+++ download.py
@@ -1,10 +1,11 @@
 from os import path
 import urllib
 
-if not path.exists('~/.vim/autoload'):
-    path.os.mkdir('~/.vim/autoload')
+downloadpath = path.expanduser('~') + '/.vim/autoload/'
+if not path.exists(downloadpath):
+    path.os.makedirs(downloadpath)
     print('Path is created')
 
 print('Downloading plug.vim')
-urllib.urlretrieve('https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim', '~/.vim/autoload/plug.vim')
+urllib.urlretrieve('https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim', downloadpath + 'plug.vim')
 print('Download finished')
